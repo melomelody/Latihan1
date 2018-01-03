bee65aad32d87d380b8b055e20aa7690af236f73
melomelody <melomelody.gm@gmail.com>
2018-01-03 13:56:21 +0700

    Perbaikan Latihan1

--- Lingkaran.java
+++ Lingkaran.java
@@ -1,8 +1,10 @@
+import java.lang.Math;
+
 public class Lingkaran{
     public static void main(String[] args){
         Integer JariJari = 15;
         Integer Diameter = JariJari*2;
-        Double Phi = 3.14;
+        Double Phi = Math.PI;
 
         System.out.println("Nilai Phi : " +Phi);
         System.out.println("Maka kelilingnya adalah : " +2*Phi*JariJari);
--- Persegi.java
+++ Persegi.java
@@ -1,6 +1,6 @@
 public class Persegi{
     public static void main(String[] args){
         java.lang.Double Sisi = 50.0;
-        System.out.println("Luas sebuah persegi dengan sisi " +Sisi+ " cm adalah : " +(Sisi * Sisi) + " cm" );
+        System.out.println("Luas sebuah persegi dengan sisi " +Sisi+ " cm adalah : " +(Sisi * Sisi/100) + " m" );
     }
 }
