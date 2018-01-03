7a5fdde62696b2456f729e0a5acedfaacfce983c
melomelody <melomelody.gm@gmail.com>
2018-01-03 13:48:24 +0700

    Latihan1

---
+++ Lingkaran.java
@@ -0,0 +1,10 @@
+public class Lingkaran{
+    public static void main(String[] args){
+        Integer JariJari = 15;
+        Integer Diameter = JariJari*2;
+        Double Phi = 3.14;
+
+        System.out.println("Nilai Phi : " +Phi);
+        System.out.println("Maka kelilingnya adalah : " +2*Phi*JariJari);
+    }
+}
---
+++ PersegiPanjang.java
@@ -0,0 +1,11 @@
+public class PersegiPanjang{
+    public static void main (String[] args){
+        java.lang.Integer Panjang = 10;
+        java.lang.Integer Tinggi = 5;
+        java.lang.Integer Luas = Panjang * Tinggi;
+
+        System.out.println("Panjang sebuah persegi panjang : " +Panjang+ " cm");
+        System.out.println("Panjang sebuah persegi panjang : " +Tinggi+ " cm");
+        System.out.println("Maka luas sebuah persegi adalah : " +Luas+ " cm");
+    }
+}
